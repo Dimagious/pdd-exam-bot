093f765210423f7af776f5fd89fa10918e44f96f
Dmitriy Yurkin <dmitrii.iurkin@yandex.ru>
2018-12-14 11:07:13 +0300

    added secret data to bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,26 @@
 # -*- coding: utf-8 -*-
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
-import config
 import messages
+
+TOKEN = '569532381:AAH5Ujv0o3pMLQsv-iHXNjHD0npmWElwztE'
+PORT = '5000'
+PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
+         'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
 
 logging.basicConfig(format=messages.LOGGING, level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 def main():
-    updater = Updater(config.TOKEN, request_kwargs=config.PROXY)
+    updater = Updater(TOKEN, request_kwargs=PROXY)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     updater.start_webhook(listen="0.0.0.0",
-                          port=config.PORT,
-                          url_path=config.TOKEN)
+                          port=PORT,
+                          url_path=TOKEN)
     updater.idle()
 
 
