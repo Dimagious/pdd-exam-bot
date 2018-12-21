29e1ea83291eba2ab873e4754f8e605b14b1790e
Dmitriy Yurkin <dmitrii.iurkin@yandex.ru>
2018-12-21 16:31:21 +0300

    added bot logic for training mode

--- bot.py
+++ bot.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 from telegram import ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, RegexHandler
 from random import randint
 import logging
 import messages
 import config
 import db
 
-CHOOSE_MODE = range(1)
 logging.basicConfig(format=messages.LOGGING, level=logging.INFO)
 logger = logging.getLogger(__name__)
 
@@ -15,8 +14,11 @@ logger = logging.getLogger(__name__)
 def main():
     updater = Updater(config.TOKEN, request_kwargs=config.PROXY)
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("start_training", start_training))
+    dp.add_handler(CommandHandler("start", start, pass_user_data=True))
+    dp.add_handler(CommandHandler("start_training", start_training, pass_user_data=True))
+    dp.add_handler(RegexHandler('^(Вернуться в меню)$', start, pass_user_data=True))
+    dp.add_handler(RegexHandler('^(Следующий вопрос️)$', start_training, pass_user_data=True))
+    dp.add_handler(CallbackQueryHandler(user_answer))
     updater.start_polling()
     # updater.start_webhook(listen="0.0.0.0",
     #                       port=PORT,
@@ -25,27 +27,93 @@ def main():
     updater.idle()
 
 
-def start(bot, update):
+def start(bot, update, user_data):
+    """
+    Метод, который представляет собой главное меню бота
+    :param bot:
+    :param update:
+    :param user_data:
+    """
     bot.sendMessage(chat_id=update.message.chat_id, text=messages.WELCOME)
 
 
-def cancel(bot, update):
-    bot.sendMessage(update.message.chat_id, messages.STOP)
-    return ConversationHandler.END
+def help(bot, update, user_data):
+    """
+    Метод, который посылает пользователю информацию о своих режимах работы и функциях
+    :param bot:
+    :param update:
+    :param user_data:
+    """
+    bot.sendMessage(chat_id=update.message.chat_id, text=messages.HELP)
 
 
-def start_training(bot, update):
-    three_answers = [[InlineKeyboardButton("1", callback_data='1'), InlineKeyboardButton("2", callback_data='2'),
-                      InlineKeyboardButton("3", callback_data='3')]]
-    four_answers = [[InlineKeyboardButton("1", callback_data='1'), InlineKeyboardButton("2", callback_data='2'),
-                     InlineKeyboardButton("3", callback_data='3'), InlineKeyboardButton("4", callback_data='4')]]
-    random_number = randint(0, 20)
-    choices = db.get_number_of_choices(0, random_number)
+def user_answer(bot, update):
+    """
+    Метод, который в зависимости от ответа пользователя использует опредеённую логику
+    :param bot:
+    :param update:
+    """
+    keyboard = [
+        [InlineKeyboardButton(messages.NEXT, callback_data='7')],
+        [InlineKeyboardButton(messages.MENU, callback_data='5')]
+    ]
 
-    reply_markup = InlineKeyboardMarkup(three_answers) if choices == 3 else InlineKeyboardMarkup(four_answers)
+    query = update.callback_query
+    user_choice = int(query.data.split(';')[0])
+    ticket = int(query.data.split(';')[1])
+    question = int(query.data.split(';')[2])
+    comment = db.get_comment(ticket, question)
+    write_choice = db.get_write_answer(ticket, question)
 
-    bot.sendMessage(chat_id=update.message.chat_id, text=db.get_question(0, random_number),
-                    reply_markup=reply_markup, one_time_keyboard=True)
+    if user_choice == 5:
+        bot.sendMessage(chat_id=query.message.chat.id, text=messages.WELCOME,
+                        reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=False))
+    elif user_choice == 6:
+        bot.sendMessage(chat_id=query.message.chat.id, text=comment,
+                        reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=False))
+    elif user_choice == 7:
+        bot.sendMessage(chat_id=query.message.chat_id, text=db.get_question(ticket, question),
+                        reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=False))
+    elif user_choice == write_choice:
+        bot.sendMessage(text='Верно! \n\n' + comment, chat_id=query.message.chat.id,
+                        message_id=query.message.message_id,
+                        reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=False))
+    else:
+        bot.sendMessage(text='Неверно! \n\n' + comment, chat_id=query.message.chat.id,
+                        message_id=query.message.message_id,
+                        reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=False))
+
+
+def start_training(bot, update, user_data):
+    """
+    Метод, который посылает случайный вопрос из случайного билета пользователю
+    :param bot:
+    :param update:
+    :param user_data:
+    """
+    random_ticket = 1
+    random_question = randint(1, 20)
+
+    three_answers = [[
+        InlineKeyboardButton(messages.ONE, callback_data='1;' + str(random_ticket) + ';' + str(random_question)),
+        InlineKeyboardButton(messages.TWO, callback_data='2;' + str(random_ticket) + ';' + str(random_question)),
+        InlineKeyboardButton(messages.THREE, callback_data='3;' + str(random_ticket) + ';' + str(random_question))
+    ]]
+
+    four_answers = [[
+        InlineKeyboardButton(messages.ONE, callback_data='1;' + str(random_ticket) + ';' + str(random_question)),
+        InlineKeyboardButton(messages.TWO, callback_data='2;' + str(random_ticket) + ';' + str(random_question)),
+        InlineKeyboardButton(messages.THREE, callback_data='3;' + str(random_ticket) + ';' + str(random_question)),
+        InlineKeyboardButton(messages.FOUR, callback_data='4;' + str(random_ticket) + ';' + str(random_question))
+    ]]
+
+    choices = db.get_number_of_choices(random_ticket, random_question)
+
+    reply_markup = InlineKeyboardMarkup(four_answers) if choices == 4 else InlineKeyboardMarkup(three_answers)
+
+    bot.sendMessage(chat_id=update.message.chat_id,
+                    text=db.get_question(random_ticket, random_question),
+                    reply_markup=reply_markup)
 
 
 if __name__ == '__main__':
