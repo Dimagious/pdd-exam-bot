6b0f6109538876b215bdfed783be42b6e4228e69
Dmitriy Yurkin <dmitrii.iurkin@yandex.ru>
2018-12-22 16:42:00 +0300

    added config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# bot
+TOKEN = '569532381:AAH5Ujv0o3pMLQsv-iHXNjHD0npmWElwztE'
+PORT = '5000'
+PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
+         'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+
+# db
+DOC_NAME = 'PDD'
+QUESTIONS = 'A2:A21'
+ANSWERS = 'B2:B21'
+COMMENTS = 'C2:C21'
+PICTURES = 'D2:D21'
+CHOICES = 'E2:E21'
+
